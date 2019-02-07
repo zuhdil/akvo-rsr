59feefe27ff848f9d20234e5b5bd050d16318115
Puneeth Chaganti <punchagan@muse-amuse.in>
2019-02-07 12:44:58 +0530

    [#3479] Don't click on the element (to expand) unless it is folded up

--- akvo/rsr/static/scripts-src/project-editor.js
+++ akvo/rsr/static/scripts-src/project-editor.js
@@ -4649,7 +4649,9 @@ function expandAccordion(highlight) {
         if (!stepInput) {
             return;
         }
-        stepInput.parentElement.firstElementChild.click();
+        if (stepInput.parentElement.querySelector(".formBlock.hidden")) {
+            stepInput.parentElement.firstElementChild.click();
+        }
         stepInput.parentElement.firstElementChild.scrollIntoView();
         location.hash = "";
     } else {
@@ -4661,7 +4663,9 @@ function expandAccordion(highlight) {
         }
         location.hash = "#" + ancestor.getAttribute("id");
         expandAccordion(highlight);
-        element.querySelector(".hide-partial-click").click();
+        if (element.querySelector(".hide-partial.hidden")) {
+            element.querySelector(".hide-partial-click").click();
+        }
         if (highlight) {
             element.classList.add("error-highlight");
             element.scrollIntoView();
